91e220dabea876fed4222bf5fff548fa13860a22
ngthvan1612 <ngthvan1612@gmail.com>
2024-04-15 20:47:12 +0700

    Update

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-print('Hello ne')
-for i in range(0, 10):
-  print("hello world")
+import os
+
+for i in range(0, 50):
+  os.system('touch a.txt')
+  os.system('git add .')
+  os.system('git commit -m "Update"')
+  os.system('git push')
+  os.system('rm -rf a.txt')
+  os.system('git add .')
+  os.system('git commit -m "Update"')
+  os.system('git push')
