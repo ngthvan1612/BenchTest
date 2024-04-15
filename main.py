99f85a26762f14459373016baf425a57a61c0494
ngthvan1612 <ngthvan1612@gmail.com>
2024-04-15 21:45:15 +0700

    Update

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-import os
+TEMPLATE = """
+  build-%d:
+    runs-on: ubuntu-latest
 
-for i in range(0, 50):
-  os.system('touch a.txt')
-  os.system('git add .')
-  os.system('git commit -m "Update"')
-  os.system('git push')
-  os.system('rm -rf a.txt')
-  os.system('git add .')
-  os.system('git commit -m "Update"')
-  os.system('git push')
+    container:
+      image: docker:stable
+    steps:
+      - name: Run
+        run: docker run -e NUM=32 ngthvan1612/bench-test:v3
+"""
+
+WORKFLOW = """
+name: CI
+
+on:
+  push:
+    branches: [ "master" ]
+  pull_request:
+    branches: [ "master" ]
+  workflow_dispatch:
+
+jobs:
+"""
+
+res = WORKFLOW
+for i in range(0, 30):
+  res += TEMPLATE.replace('%d', str(i + 1))
+
+with open('.github/workflows/blank.yml', 'w') as f:
+  f.write(res)
